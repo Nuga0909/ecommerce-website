9b35c198961b5b748cd7fab179f096c642542402
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-14 20:28:35 +0100

    Add introducing model

---
+++ frontend/src/components/Home/IntroducingModel.js
@@ -0,0 +1,180 @@
+import React from 'react'
+
+function IntroducingModel() {
+  return (
+    <div className="w-[1440px] h-[3234px] px-[135px] py-20 bg-slate-950 justify-start items-start gap-2.5 inline-flex">
+      <div className="flex-col justify-start items-start gap-20 inline-flex">
+        <div className="flex-col justify-start items-start gap-10 flex">
+          <div className="flex-col justify-start items-center gap-12 flex">
+            <div className="px-6 py-[17px] bg-white rounded-[32px] justify-center items-start gap-2.5 inline-flex">
+              <div className="text-center text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                New
+              </div>
+            </div>
+            <div className="w-[1170px] text-center text-white text-7xl font-bold font-['Noto Serif Lao'] leading-[80px]">
+              Introducing Model 001
+            </div>
+          </div>
+          <img
+            className="w-[1170px] h-[702px]"
+            src="https://via.placeholder.com/1170x702" alt=''
+          />
+        </div>
+        <div className="flex-col justify-start items-start gap-10 flex">
+          <div className="w-[1170px] text-white text-[28px] font-bold font-['Noto Serif Lao'] leading-loose">
+            Explore Model 001
+          </div>
+          <div className="justify-start items-start gap-[30px] inline-flex">
+            <div className="flex-col justify-start items-start gap-[30px] inline-flex">
+              <div className="justify-start items-start inline-flex">
+                <div className="px-6 pt-20 pb-6 bg-zinc-100 rounded-2xl justify-start items-start gap-2.5 flex">
+                  <div className="flex-col justify-start items-start gap-6 inline-flex">
+                    <img
+                      className="w-[322px] h-[270px] mix-blend-darken rounded-2xl"
+                      src="https://via.placeholder.com/322x270" alt=''
+                    />
+                    <div className="flex-col justify-start items-start gap-2 flex">
+                      <div className="w-[322px] text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                        Model 000
+                      </div>
+                      <div className="justify-start items-start gap-4 inline-flex">
+                        <div className="w-[243px] text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          White
+                        </div>
+                        <div className="w-[63px] text-right text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          $145
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="justify-start items-start inline-flex">
+                <div className="px-6 pt-20 pb-6 bg-zinc-100 rounded-2xl justify-start items-start gap-2.5 flex">
+                  <div className="flex-col justify-start items-start gap-6 inline-flex">
+                    <img
+                      className="w-[322px] h-[270px] mix-blend-darken rounded-2xl"
+                      src="https://via.placeholder.com/322x270" alt=''
+                    />
+                    <div className="flex-col justify-start items-start gap-2 flex">
+                      <div className="w-[322px] text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                        Model 000
+                      </div>
+                      <div className="justify-start items-start gap-4 inline-flex">
+                        <div className="w-[243px] text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          Black
+                        </div>
+                        <div className="w-[63px] text-right text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          $145
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[770px] h-[922px] relative">
+              <img
+                className="w-[770px] h-[922px] left-0 top-0 absolute rounded-2xl"
+                src="https://via.placeholder.com/770x922" alt=''
+              />
+              <div className="left-[24px] top-[846px] absolute justify-start items-end gap-[276px] inline-flex">
+                <div className="flex-col justify-start items-start gap-2 inline-flex">
+                  <div className="w-[322px] text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                    Model 000
+                  </div>
+                  <div className="w-[322px] text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                    Navy
+                  </div>
+                </div>
+                <div className="px-6 py-[17px] bg-lime-300 rounded-[32px] justify-center items-start gap-2.5 flex">
+                  <div className="text-center text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                    Shop Now
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-col justify-start items-start gap-10 flex">
+          <div className="w-[1170px] text-white text-[28px] font-bold font-['Noto Serif Lao'] leading-loose">
+            Accessoies
+          </div>
+          <div className="justify-start items-start gap-[30px] inline-flex">
+            <div className="w-[770px] h-[922px] relative">
+              <img
+                className="w-[770px] h-[922px] left-0 top-0 absolute rounded-2xl"
+                src="https://via.placeholder.com/770x922" alt=''
+              />
+              <div className="left-[24px] top-[846px] absolute justify-start items-end gap-[276px] inline-flex">
+                <div className="flex-col justify-start items-start gap-2 inline-flex">
+                  <div className="w-[322px] text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                    Model 000
+                  </div>
+                  <div className="w-[322px] text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                    Black
+                  </div>
+                </div>
+                <div className="px-6 py-[17px] bg-lime-300 rounded-[32px] justify-center items-start gap-2.5 flex">
+                  <div className="text-center text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                    Shop Now
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex-col justify-start items-start gap-[30px] inline-flex">
+              <div className="justify-start items-start inline-flex">
+                <div className="px-6 pt-20 pb-6 bg-zinc-100 rounded-2xl justify-start items-start gap-2.5 flex">
+                  <div className="flex-col justify-start items-start gap-6 inline-flex">
+                    <img
+                      className="w-[322px] h-[270px] mix-blend-darken rounded-2xl"
+                      src="https://via.placeholder.com/322x270" alt=''
+                    />
+                    <div className="flex-col justify-start items-start gap-2 flex">
+                      <div className="w-[322px] text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                        Atoms Stretch Laces
+                      </div>
+                      <div className="justify-start items-start gap-4 inline-flex">
+                        <div className="w-[243px] text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          Black
+                        </div>
+                        <div className="w-[63px] text-right text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          $12
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="justify-start items-start inline-flex">
+                <div className="px-6 pt-20 pb-6 bg-zinc-100 rounded-2xl justify-start items-start gap-2.5 flex">
+                  <div className="flex-col justify-start items-start gap-6 inline-flex">
+                    <img
+                      className="w-[322px] h-[270px] mix-blend-darken rounded-2xl"
+                      src="https://via.placeholder.com/322x270" alt=''
+                    />
+                    <div className="flex-col justify-start items-start gap-2 flex">
+                      <div className="w-[322px] text-slate-950 text-base font-normal font-['Rubik'] leading-snug">
+                        Atoms Everyday Mask
+                      </div>
+                      <div className="justify-start items-start gap-4 inline-flex">
+                        <div className="w-[243px] text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          Black
+                        </div>
+                        <div className="w-[63px] text-right text-slate-500 text-sm font-normal font-['Rubik'] leading-[18px]">
+                          $12
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IntroducingModel
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,6 +4,7 @@ import Hero from "../components/Home/Hero";
 import ContentOne from "../components/Home/ContentOne";
 import ExploreShoes from "../components/Home/ExploreShoes";
 import TheHype from "../components/Home/TheHype";
+import IntroducingModel from "../components/Home/IntroducingModel";
 
 function HomePage() {
   return (
@@ -12,7 +13,8 @@ function HomePage() {
       <Hero />
       <ContentOne />
       <ExploreShoes />
-      <TheHype/>
+      <TheHype />
+      <IntroducingModel/>
     </div>
   );
 }
