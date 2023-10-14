c82ddfa87c36b97266b6ffd48416adefc821b4a5
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-14 20:14:12 +0100

    Add content one to home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavigationBar from '../components/Home/NavigationBar';
 import Hero from '../components/Home/Hero';
+import ContentOne from '../components/Home/ContentOne';
 
 function HomePage() {
   return (
       <div>
           <NavigationBar />
           <Hero />
+          <ContentOne />
     </div>
   );
 }
