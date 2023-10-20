56e5548d3710a16631fa5b6a062181f8df7846da
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-20 16:02:40 +0100

    Add validation to product .route

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,28 +1,46 @@
-import express from 'express';
-const router = express.Router();
-import { getAllProducts, getSingleProduct, postProduct } from '../controller/products.controller.js';
+import express from "express";
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
+import { check, validationResult } from "express-validator";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../../uploads"));
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
-  },
+const router = express.Router();
+const s3 = new aws.S3({
+  /* your AWS S3 config here */
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "your-bucket-name",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, uuidv4() + "-" + file.originalname);
+    },
+  }),
+});
 
-router.get('/all', getAllProducts);
+// Validation middleware for posting a product
+const validateProductPost = [
+  check("category").not().isEmpty().trim().escape(),
+  check("brand").not().isEmpty().trim().escape(),
+  check("model").not().isEmpty().trim().escape(),
+  check("price").isNumeric(),
+  check("inStock").isNumeric(),
+  // You can add more checks as needed
+];
 
-router.get('/single/:id', getSingleProduct);
-
-router.post("/post-product", upload.single("image"), postProduct);
+router.get("/all", getAllProducts);
+router.get("/single/:id", getSingleProduct);
+router.post(
+  "/post-product",
+  validateProductPost,
+  upload.single("image"),
+  postProduct
+);
 
 router.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
