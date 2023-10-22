70d0566808444d97b681cf5171ef94c27a373b02
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-22 17:10:46 +0100

    revert commits 2

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,46 +1,32 @@
 import express from "express";
-import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk";
-import { v4 as uuidv4 } from "uuid";
-import { check, validationResult } from "express-validator";
-
 const router = express.Router();
-const s3 = new aws.S3({
-  region: process.env.AWS_REGION,
+import {
+  getAllProducts,
+  getSingleProduct,
+  postProduct,
+} from "../controller/products.controller.js";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  },
 });
 
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "your-bucket-name",
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      cb(null, uuidv4() + "-" + file.originalname);
-    },
-  }),
-});
-
-// Validation middleware for posting a product
-const validateProductPost = [
-  check("category").not().isEmpty().trim().escape(),
-  check("brand").not().isEmpty().trim().escape(),
-  check("model").not().isEmpty().trim().escape(),
-  check("price").isNumeric(),
-  check("inStock").isNumeric(),
-  // You can add more checks as needed
-];
+const upload = multer({ storage: storage });
 
 router.get("/all", getAllProducts);
+
 router.get("/single/:id", getSingleProduct);
-router.post(
-  "/post-product",
-  validateProductPost,
-  upload.single("image"),
-  postProduct
-);
+
+router.post("/post-product", upload.single("image"), postProduct);
 
 router.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
